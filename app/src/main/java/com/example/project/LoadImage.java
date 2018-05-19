029e2a6f964119d02781a9390914891b24968a83
Roberto Deresu <ioan-roberto.deresu@my.fmi.unibuc.ro>
2018-05-19 22:37:09 +0300

    Helpers.

---
+++ app/src/main/java/com/example/project/LoadImage.java
@@ -0,0 +1,18 @@
+package com.example.proiect;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Alex Constantin on 4/8/2017.
+ */
+
+public class LoadImage {
+}
